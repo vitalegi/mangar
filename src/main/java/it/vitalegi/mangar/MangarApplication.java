58b9992d09be0c4d3ebb5d4fdd7a23daaf664ef5
Giorgio Vitale <giorgio.vitale.90@gmail.com>
2023-02-20 19:22:45 +0100

    init

---
+++ src/main/java/it/vitalegi/mangar/MangarApplication.java
@@ -0,0 +1,13 @@
+package it.vitalegi.mangar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/vitalegi/mangar/MangarApplicationTests.java
@@ -0,0 +1,13 @@
+package it.vitalegi.mangar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
